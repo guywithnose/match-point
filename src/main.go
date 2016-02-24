641aa9643a5954c9f405d3e4a4d4ae919b105abd
Robert Bittle <guywithnose@gmail.com>
2016-02-23 20:07:04 -0500

    Update icons

--- src/main.go
+++ src/main.go
@@ -9,6 +9,8 @@ import (
     "encoding/json"
     "bytes"
     "io/ioutil"
+    "regexp"
+    "strconv"
 )
 
 var (
@@ -78,6 +80,19 @@ type GcmResult struct {
     MessageId string `json:"message_id"`
 }
 
+type Manifest struct {
+    Name string `json:"name"`
+    GcmSenderId string `json:"gcm_sender_id"`
+    Icons []ManifestIcon `json:"icons"`
+}
+
+type ManifestIcon struct {
+    Source string `json:"src"`
+    Size string `json:"sizes"`
+    Type string `json:"type"`
+    Density float64 `json:"density"`
+}
+
 func main() {
     r := mux.NewRouter()
     r.HandleFunc("/ws", Socket(handleMessage))
@@ -89,7 +104,33 @@ func main() {
 }
 
 func buildManifest(w http.ResponseWriter, r *http.Request) {
-    json, err := json.Marshal(map[string]string{"name": "Match Point", "gcm_sender_id": gcmSenderId})
+    var manifest Manifest = Manifest{
+        Name: "Match Point",
+        GcmSenderId: gcmSenderId,
+        Icons: make([]ManifestIcon, 0),
+    }
+
+    files, err := ioutil.ReadDir("public/images")
+    if err != nil {
+        log.Println(err.Error());
+    }
+
+    for key := range(files) {
+        fileName := files[key].Name()
+        re := regexp.MustCompile("soccerIcon-([0-9]+)x([0-9]+)-([0-9\\.]+)x.png")
+        matches := re.FindAllStringSubmatch(fileName, -1)
+        if matches != nil {
+            density, _ := strconv.ParseFloat(matches[0][3], 64)
+            manifest.Icons = append(manifest.Icons, ManifestIcon{
+                Source: "/images/" + fileName,
+                Size: matches[0][1] + "x" + matches[0][2],
+                Type: "image/png",
+                Density: density,
+            })
+        }
+    }
+
+    json, err := json.Marshal(manifest)
     if err != nil {
         log.Println(err)
     }
